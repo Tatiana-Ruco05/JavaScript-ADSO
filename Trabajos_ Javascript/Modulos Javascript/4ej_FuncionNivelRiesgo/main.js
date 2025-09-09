e5cc6621887a380bf9423db8ba332fcd601b9ac1
Tatiana-Ruco05 <sena.tatiana05@gmail.com>
2025-09-09 07:04:09 -0500

    edit

--- Trabajos_ Javascript/Modulos Javascript/4ej_FuncionNivelRiesgo/main.js
+++ Trabajos_ Javascript/Modulos Javascript/4ej_FuncionNivelRiesgo/main.js
@@ -2,4 +2,4 @@ import { nivelRiesgo } from './utilidades.js';
 
 console.log(nivelRiesgo(65, false));  // un Alto riesgo
 console.log(nivelRiesgo(50, true));   // un Riesgo moderado
-console.log(nivelRiesgo(30, false));  // un Bajo riesgo
+console.log(nivelRiesgo(30, false));  // un Bajo riesgo 
